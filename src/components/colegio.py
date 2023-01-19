9e1962aa09895b5d21c5cb4ac131712a1beaa0ac
Rompe000 <dacassan2@gmail.com>
2023-01-19 20:18:41 +0100

    Properties y clases nuevas + 9 asignaturas

--- src/components/colegio.py
+++ src/components/colegio.py
@@ -1,12 +1,38 @@
+
+class Asignatura:
+
+    _nombre: str
+    _horasSemanales: int
+
+    def __init__(self, nombre:str, horasSemanales:int) -> None:
+        self._nombre = nombre
+        self._horasSemanales = horasSemanales
+        pass
+
+    def Nombre(self, value:str=None):
+        if value != None:
+            self._nombre = value 
+        return self._nombre
+
+    def HorasSemanales(self, value:int=None):
+        if value != None:
+            self._horasSemanales = value 
+        return self._horasSemanales
+
+
+
 class Profesor:
     
     _nombre: str
     _tutor: bool
     _horario:list[int,int]
-    _asignaturas: list[str]
+    _asignaturas: list[Asignatura]
 
-    def __init__(self):
-        self._nombre = "Jamon"
+    def __init__(self, tutor:bool, nombre:str, horario:list[int,int], asignaturas:list[Asignatura]):
+        self._tutor = tutor
+        self._nombre = nombre
+        self._horario = horario
+        self._asignaturas = asignaturas
 
     def Nombre(self, value:str=None):
         if value != None:
@@ -22,19 +48,12 @@ class Profesor:
             self._horario = value 
         return self._horario
     
-    def Asignaturas(self, value:list[str]=None):
+    def Asignaturas(self, value:list[Asignatura]=None):
         if value != None:
             self._asignaturas = value 
         return self._asignaturas
 
 
-class Asignatura:
-
-    _nombre: str
-    _horasSemanales: int
-
-    def __init__(self) -> None:
-        pass
 
 
 class Grupo:
@@ -46,3 +65,33 @@ class Grupo:
 
     def __init__(self) -> None:
         pass
+
+
+    def Asignaturas(self, value:list[Asignatura]=None):
+        if value != None:
+            self._asignaturas = value 
+        return self._asignaturas
+
+
+    def Tutor(self, value:Profesor=None):
+        if value != None:
+            self._tutor = value 
+        return self._tutor
+
+
+    def Profesores(self, value:list[Profesor]=None):
+        if value != None:
+            self._profesores = value 
+        return self._profesores
+
+    def Horario(self, value:str=None):
+        if value != None:
+            self._horario = value 
+        return self._horario
+
+
+class Hora_horario:
+    _nombre: Asignatura
+    _hora: list[str,str]
+    _grupo: Grupo
+    _profesor: Profesor
