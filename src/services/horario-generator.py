44b04d9fbbef846090a9e98fef258371f8848e85
Rompe000 <dacassan2@gmail.com>
2023-01-19 20:19:02 +0100

    9 asignaturas

---
+++ src/services/horario-generator.py
@@ -0,0 +1,14 @@
+from src.components.colegio import *
+
+asig1 = Asignatura("Moviles", 4)
+asig2 = Asignatura("Servicios y procesos", 7)
+asig3 = Asignatura("Desarrollo de Interfaces", 3)
+asig4 = Asignatura("Ingles", 2)
+asig5 = Asignatura("Empersa", 3)
+asig6 = Asignatura("Navision", 3)
+asig7 = Asignatura("Amazon", 2)
+asig8 = Asignatura("Acceso a datos", 5)
+asig9 = Asignatura("Big data", 2)
+
+def generar() -> list[Hora_horario, Hora_horario]:
+    print("Hola")
