77e7bac9a1aea3bdc15e39bb6767a8239580e296
Erojase <Erojase>
2023-01-25 20:02:18 +0100

    added login initial and comments

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import jwt
+from datetime import datetime, timedelta
 from src.services.serviceManager import ServiceManager
 from src.services.dbManager import DbManager
 
@@ -24,6 +26,20 @@ def calendario():
 def testAction():
     print("Test Action Triggered")
     return "Test Action Triggered"
+
+@application.route('/login', methods=['POST'])
+def login():
+    data = request.get_json(silent=True)
+    request.headers
+    # Comprobar si existe y esas cosas
+    tokenData = {
+        "id": 1,
+        "exp": datetime.utcnow() + timedelta(seconds=1) #expira en 1 segundo
+    }
+    SECRET_KEY = "JOYFE"
+    return jwt.encode(tokenData, SECRET_KEY, algorithm='HS256') # Exactamente asi es en encode
+    #      try: porque cuando no se decodea lanza una excepcion
+    #         jwt.decode(tokenData, SECRET_KEY, algorithms=['HS256']) Exactamente asi es el decode
 
 if __name__ == '__main__':
     application.run(debug=True,host='0.0.0.0')
