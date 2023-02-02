a448ec636e7b3106122e468393e0993b5bae10e6
Erojase <Erojase>
2023-02-02 16:30:46 +0100

    test for david

--- static/scripts/calendar.js
+++ static/scripts/calendar.js
@@ -2,7 +2,15 @@ let headersList = {
     "Accept": "*/*",
     "Content-Type": "application/json"
 }
+const dias = ["Lunes","Martes","Miercoles","Jueves","Viernes"]
+let selector;
+let grupos = {}
 
+document.addEventListener('DOMContentLoaded', ()=>{
+    selector = document.getElementById("group");
+    selector.addEventListener('change', cargarGrupo);
+
+});
 
 async function testData() {
     let response = await fetch('/calTest', { 
@@ -11,7 +19,7 @@ async function testData() {
     });
 
     let data = await response.text();
-    console.log(JSON.parse(data));
+    // console.log(JSON.parse(data));
     loadTable(JSON.parse(data))
 }
 
@@ -19,17 +27,30 @@ function loadTable(data){
     // semana data[0]
     // dia data[0][0]
     // hora data [0][0][0]
-    let selector = document.getElementById("group");
-    let grupos = []
+    
     data.forEach(grupo => {
+        grpName = JSON.parse(grupo[0][0])['grupo'];
         let opt = document.createElement('option');
-        opt.value = JSON.parse(grupo[0][0])['grupo'];
-        opt.text = JSON.parse(grupo[0][0])['grupo'];
+        opt.value = grpName;
+        opt.text = grpName;
         selector.appendChild(opt);
+
+        grupos[grpName] = grupo;
     });
+    console.log(grupos);
+    let cont = 0;
+    for (const grupo in grupos) {
+        console.log(grupo);
+        cont = 0;
+        grupos[grupo].forEach(dia => {
+            grupos[grupo][dias[cont]] = dia;
+            delete grupos[grupo][cont];
+            cont++;
+        });
+    }
+}
 
-
-
-
-
+function cargarGrupo() {
+    let currgrp = grupos[selector.value];
+    
 }
