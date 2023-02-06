abb10c41476f49a763acf65f0c7a9da273e82178
ITZAIDES8 <bermudez.sebas2008@gmail.com>
2023-02-06 20:17:14 +0100

    taskerhtml,py,css,js/2

--- src/services/tasker.py
+++ src/services/tasker.py
@@ -3,11 +3,44 @@ from src.components.users import *
 
 
 
+def __init__(self) -> None:
+        pass
+    
+    
+    
+# --------------------------------------------------------------------------------------------
+def getUser(self, id:int) -> list[dict]:
+        coll = self.database["Usuarios"]
+        for item in coll.find({"id":id}):
+            return item
+    
+def insertUser(self, usuario:Usuario):
+        insertDict = usuario.toJson()
+        for user in self.listUsers():
+            if user["id"] == insertDict["id"]:
+                return "Not a valid id"
+        coll = self.database["Usuarios"]
+        coll.insert_one(insertDict)
+# ----------------------------------------------------------------------------------------
+
 
  # profesores publican tareas
 def crearTarea():
     
+    
+    
     return 
+
+
+# alumno sube respuesta de la tarea
+def subirTarea():
+    
+    
+    
+    
+    return
+
+
 
 #profesores eliminan tareas
 def eliminarTarea():
@@ -17,10 +50,6 @@ def eliminarTarea():
 def imprimeTarea():
     return
 
-# alumno sube respuesta de la tarea
-def subirTarea():
-    
-    return
 
 
 
--- static/scripts/tasker.js
+++ static/scripts/tasker.js
@@ -7,3 +7,29 @@ document.addEventListener('DOMContentLoaded', ()=>{})
 
 
 
+async function subir() {
+let tarea = document.getElementById("tarea").value;
+let titulo = document.getElementById("titulo").value;
+let content = JSON.stringify({
+    "tarea": tarea, //esto lo coje el html
+    "titulo": titulo //esto hatml
+})
+
+
+}
+
+
+    // let username = document.getElementById("name").value;
+    // let password = MD5.generate(document.getElementById("passwd").value)
+    // let content = JSON.stringify({
+    //     "user": username,
+    //     "password": password
+    // });
+    // let response = await fetch('/login', { 
+    //     method: "POST",
+    //     headers: headersList,
+    //     body: content 
+    // });
+
+    // let data = await response.text();
+    // console.log(data);
