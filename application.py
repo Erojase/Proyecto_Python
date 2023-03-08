70c3db7d2f0cf4c46411a4551b02cb7c92b69544
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2023-03-08 20:25:50 +0100

    reformat

--- application.py
+++ application.py
@@ -28,14 +28,14 @@ def googleAuth():
     data = parseToken(request.get_data().decode(encoding="utf-8").split('=')[1].split('&')[0], False) 
     tkdata = {}
     token = ""
-    for user in db.listUsers():
-        if user["mail"] == data["email"]:
-            token = {"exp": dt.utcnow() + timedelta(days=1)} #expira en 1 dia
-            tkdata["id"] = user["id"]
-            tkdata["tipo"] = user["tipo"]
-            tkdata["mail"] = user["mail"]
-            tkdata["nick"] = user["nick"]
-            token.update(tkdata)
+    
+    user = db.getOneUser({"mail":data["email"]})
+    if len(user) > 0:
+        token = {"exp": dt.utcnow() + timedelta(days=1)} #expira en 1 dia
+        tkdata["tipo"] = user["tipo"]
+        tkdata["mail"] = user["mail"]
+        tkdata["nick"] = user["nick"]
+        token.update(tkdata)
     if token == "":
         return "<html><script>window.localStorage.removeItem('token'); window.location.href = '/login'; </script></html>"
     codedToken = jwt.encode(token, SECRET_KEY, algorithm='HS256')
@@ -52,22 +52,17 @@ def login():
     if "user" not in data or "password" not in data:
         return jsonify("Expected more from you"), 400
     
-    # nombre_de_profesor = data["nombre"]
-    
-    for user in db.listUsers():
-        if user["nick"] == data["user"] and user["password"] == data["password"]:
-            tokenData = {"exp": dt.utcnow() + timedelta(days=1)} #expira en 1 dia
-            data["id"] = user["id"]
-            data["mail"] = user["mail"]
-            data["tipo"] = user["tipo"] 
-            del data['password']
-            tokenData.update(data)
-            return jwt.encode(tokenData, SECRET_KEY, algorithm='HS256') # Exactamente asi es en encode
+    user = db.getOneUser({"user":data["username"], "password":data["password"]})
+    if len(user) > 0:
+        tokenData = {"exp": dt.utcnow() + timedelta(days=1)} #expira en 1 dia
+        data["id"] = user["id"]
+        data["mail"] = user["mail"]
+        data["tipo"] = user["tipo"] 
+        del data['password']
+        tokenData.update(data)
+        return jwt.encode(tokenData, SECRET_KEY, algorithm='HS256')
             
     return jsonify("We do not do that here"), 400        
-    #      try: porque cuando no se decodea lanza una excepcion
-    #         jwt.decode(tokenData, SECRET_KEY, algorithms=['HS256']) Exactamente asi es el decode
-# --------------------------------------------------------------------------------------------------
     
 @application.route('/register', methods=['POST'])
 def register():
@@ -75,16 +70,11 @@ def register():
     if "user" not in data or "password" not in data:
         return jsonify("Expected more from you"), 400
     
-    maxId = 0
-    for user in db.listUsers():
-        if user["nick"] == data["user"]:
-            return jsonify("We do not do that here"), 400 
-        if user["id"] > maxId:
-            maxId = user["id"]
+
     if data["tipo"] == Tipo.Alumno.name: 
-        tmpUsr:Usuario = Usuario(maxId + 1, data["user"], "", "", data["password"], data["mail"], Tipo.Alumno)
+        tmpUsr:Usuario = Usuario(None, data["user"], "", "", data["password"], data["mail"], Tipo.Alumno)
     else:
-        tmpUsr:Usuario = Usuario(maxId + 1, data["user"], "", "", data["password"], data["mail"], Tipo.Profesor)
+        tmpUsr:Usuario = Usuario(None, data["user"], "", "", data["password"], data["mail"], Tipo.Profesor)
     return  db.insertUser(tmpUsr)
 
 @application.route('/testAction', methods=['POST'])
@@ -99,80 +89,10 @@ def calendario():
 
 
 
-# ---------------------------------------------------------------------------------------------------
-# tasker
-
-@application.route("/tasker",methods=['GET'])
-def taskerr():
-    
-    tipo = parseToken(request.args.get('token'))
-    if tipo['tipo'] == Tipo.Profesor.name:
-         return open('pages/tasker.html', 'r', encoding='utf-8')
-    elif tipo['tipo'] == Tipo.Alumno.name:
-        return open('pages/deliver.html', 'r', encoding='utf-8')
-        # return open('pages/tasker.html', 'r', encoding='utf-8')
-    
-    
-
-@application.route("/tasker",methods=['POST'])
-def tasker():
-    token = request.headers['Authorization'].split()[1]
-    user = parseToken(token)
-    data = request.get_json(silent=True)
-    
-    tarea = crearTarea(data,user)
-    if "titulo" not in data or "tarea" not in data:
-        return jsonify("Expected more from you"), 400
-    
-    db.insertTarea(tarea)
-    return 200
-
-@application.route("/tasker/getTask",methods=['POST'])
-def getTareas():
-    token = request.headers['Authorization'].split()[1]
-    user = parseToken(token)
-    
-    if user['tipo'] == Tipo.Alumno.name :
-        return jsonify(db.getTareas(user['mail']))
-    
-
-
-# revisar
-@application.route("/deliver",methods=['POST'])
-def deliver():
-    token = request.headers['Authorization'].split()[1]
-    user = parseToken(token)
-    tokenData = parseToken(token)
-    data = request.get_json(silent=True)
-    
-    clave = request.headers["clave"]
-    iarchiv = request.files.get("archiv")
-
-    Tarea = subirTarea(data,user)
-    
-    db.subir_tarea(Tarea)
-    return 200
-
-
-
-# ---------------------------------------------------------------------------------------------------
-
-
-
-
 @application.route('/calTest', methods=['GET'])
 def calTest():
     return sm.CalendarTestRun()
 
-@application.route("/ponerTarea", methods=['POST'])
-def ponerTatea():
-    # data = request.get_json(silent=True)
-    # sm.ponerTarea(data['Tarea'])
-    return "Not yet implemented"
-
-@application.route('/report', methods=['GET'])
-def report():
-    return "Not yet implemented"
 
 @application.route('/attendance', methods=['GET'])
 def asistencia():
@@ -207,23 +127,15 @@ def crear_buscar_clase():
                 # return jsonify(clase.toJson())
         return ''
 
-@application.route('/attendance/getclas', methods=['POST'])
+@application.route('/attendance/getclass', methods=['POST'])
 def getclase():
     token = request.headers["Authorization"].split()[1]
     tipo = parseToken(token)
     if sm.Buscar_clase_profe(tipo['nick']) != None:
         return jsonify(sm.Buscar_clase_profe(tipo['nick']).toJson())
     return ''
-
-@application.route('/attendance/hechar', methods=['POST'])
-def hechar():
-    token = request.headers["Authorization"].split()[1]
-    tipo = parseToken(token)
-    data = request.get_json(silent=True)
     
-    return sm.Hechar_de_clase(tipo['nick'], data)
-    
-@application.route('/attendance/fich', methods=['POST'])
+@application.route('/attendance/fichero', methods=['POST'])
 def crear_fich():
     token = request.headers["Authorization"].split()[1]
     tipo = parseToken(token)
@@ -233,7 +145,7 @@ def crear_fich():
     file = open(f'{PATH_BASE}/static/attender/Asistencia.txt','w')  
     if data == 'Presente':
         cont:int = 0
-        for al in clase.Alumnos():
+        for al in clase.alumnos:
             if clase.Checked()[cont] == 1:
                 comp:str = al.split('@')[0]
                 apellidios:str = comp.split('.')[1] + ' ' + comp.split('.')[2]
@@ -242,7 +154,7 @@ def crear_fich():
             cont += 1
     else:
         cont:int = 0
-        for al in clase.Alumnos():
+        for al in clase.alumnos:
             if clase.Checked()[cont] == 0:
                 comp:str = al.split('@')[0]
                 apellidios:str = comp.split('.')[1] + ' ' + comp.split('.')[2]
@@ -258,24 +170,9 @@ def getGrupos():
     return jsonify(db.getGrupos())
 
 
-@application.route('/parking', methods=['GET'])
-def parking():
-    return "Not yet implemented"
-
 @application.route('/bot', methods=['GET'])
 def discord():
     return open('pages/bot.html', 'r', encoding='utf-8')
-
-@application.route('/mail', methods=['GET'])
-def mail():
-    return open('pages/mail.html', 'r', encoding='utf-8')
-
-@application.route('/sendMail', methods=['POST'])
-def sendMail():
-    data = request.get_json(silent=True)
-    return "Not yet implemented"
-    
-
 @application.route('/listUsers', methods=['GET'])
 def listUsers():
     users = db.listUsers()
@@ -300,6 +197,9 @@ def parseToken(token:str, verify_signature=True):
     return data
 
 def purgeTmpDirs():
+    if not os.path.exists(os.getcwd()+'\\static\\attender\\'):
+        os.mkdir(os.getcwd()+'\\static\\attender\\')
+        
     dirlist = os.listdir(os.getcwd()+'\\static\\attender\\')
     for dir in dirlist:
         os.remove(os.getcwd()+'\\static\\attender\\'+dir)
