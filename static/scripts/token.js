c5a93032cb7d4b29c1314802ba48e68f2563be46
PinillaJoyfe <carlos.pinilla.bernal@alumnojoyfe.iepgroup.es>
2023-03-08 18:17:13 +0100

    Revert "ultima_face"
    This reverts commit b5d086d1078b299f90a2a4e1aefd98975771eb1a.

--- static/scripts/token.js
+++ static/scripts/token.js
@@ -33,50 +33,23 @@ function replaceLoginWithName(data) {
 function click(event) {
     event.preventDefault();
     if (!toggleSession) {
+        let context = document.createElement("div");
+    context.style.backgroundColor = "#242424";
+    context.style.color = "white";
+    context.style.position = "absolute";
+    context.style.marginTop = "10px";
+    context.style.padding = "10px";
+    context.style.borderRadius = "5px";
+    context.innerHTML = "Cerrar Sesion";
+    context.id = "cerrarSesion";
 
-        let btnLogout = document.createElement("div");
-        btnLogout.style.backgroundColor = "#242424";
-        btnLogout.style.color = "white";
-        btnLogout.style.position = "relative";
-        btnLogout.style.marginTop = "10px";
-        btnLogout.style.padding = "10px";
-        btnLogout.style.borderRadius = "5px";
-        btnLogout.innerHTML = "Cerrar Sesion";
-        btnLogout.id = "cerrarSesion";
+    context.addEventListener("click", ()=>{
+        logout();
+    });
 
-        btnLogout.addEventListener("click", ()=>{
-            logout();
-        });
-
-        let btnInfo = document.createElement("div");
-        btnInfo.style.backgroundColor = "#242424";
-        btnInfo.style.color = "white";
-        btnInfo.style.position = "relative";
-        btnInfo.style.marginTop = "10px";
-        btnInfo.style.padding = "10px";
-        btnInfo.style.borderRadius = "5px";
-        btnInfo.innerHTML = "Info";
-        btnInfo.id = "editInfo";
-
-        btnInfo.addEventListener("click", ()=>{
-            info();
-        });
-
-        let div = document.createElement("div");
-        div.style.backgroundColor = "#242424";
-        div.style.color = "white";
-        div.style.position = "absolute";
-        div.style.marginTop = "10px";
-        div.style.padding = "10px";
-        div.style.borderRadius = "5px";
-        div.id = "containeroo";
-
-        div.appendChild(btnInfo);
-        div.appendChild(btnLogout);
-        document.getElementById("loginAnchor").appendChild(div);
-       
+    document.getElementById("loginAnchor").appendChild(context)
     } else{
-        document.getElementById("containeroo").remove();
+        document.getElementById("cerrarSesion").remove();
     }
     toggleSession = !toggleSession;
     
@@ -84,9 +57,5 @@ function click(event) {
 
 function logout() {
     window.localStorage.removeItem("token");
-    window.location.href = "/web";
-}
-
-function info() {
-    window.location.href = "/info";
+    window.location.href = window.location.href;
 }
